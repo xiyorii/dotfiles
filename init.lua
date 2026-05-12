4e441a4feea42973f152c14cd871e552c33233ea
xiyorii <morales.armando2004@gmail.com>
2026-05-12 12:41:42 +0000

    Add initial Neovim configuration in init.lua

---
+++ init.lua
@@ -0,0 +1,21 @@
+# NVIM CONFIG
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("tokyonight")
+        end,
+    },
+})
